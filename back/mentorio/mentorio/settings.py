20e4e10d35787cc658262ac0ccfaecae2d8871c9
Emil Akperov <emil-akper@yandex.ru>
2024-04-27 12:44:53 +0300

    First steps in routing and admin-panel

--- back/mentorio/mentorio/settings.py
+++ back/mentorio/mentorio/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'mentoriodb',
         'USER': 'root',
-        'PASSWORD': '<YOUR-PASSWORD>',
+        'PASSWORD': '1234',
         'HOST': 'localhost',   
         'PORT': '3306',
     }    
