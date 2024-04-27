33b078cfd3d48b07b7135908aaf105bef2a02371
Maksim Goncharovskiy <maksimgoncharovskiy@gmail.com>
2024-04-27 15:43:49 +0300

    Сделана страница с регистрацией ментора

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import {Routes, Route, Link } from 'react-router-dom';
 import { Homepage } from './include/Homepage';
 import { MentorPage } from './include/MentorPage';
+import {SignUpPage} from './include/SignUpPage';
 
 
 function App() {
   return (
-    <div className="App" class="container">
+    <div className="App" class="container" id="block">
 
       <div class="row nav-panel">
         <div class="col-md-12 col-lg-2"><a href="/" id="title">Mentorio</a></div>
@@ -18,14 +19,14 @@ function App() {
 
         <div class="col-lg-2"></div>
   
-        <div class="col-md-6 col-sm-12 col-xs-12 col-lg-2"><a href="/mentors" class="nav-item">Cтать ментором</a></div>
+        <div class="col-md-6 col-sm-12 col-xs-12 col-lg-2"><a href="/sign-up" class="nav-item">Cтать ментором</a></div>
 
         <div class="col-md-6 col-sm-12 col-xs-12 col-lg-2"><a href="#" class="nav-item">Вход</a></div>
       </div>
-  
+
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
-        <Route path="/mentors" element={<MentorPage />}></Route>
+        <Route path="/sign-up" element={<SignUpPage />}></Route>
       </Routes>
     </div>
   );
